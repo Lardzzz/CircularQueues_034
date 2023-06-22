a325b886811106f9bdf378d5efc4bba47702335d
Lardzzz <aku.ikan04@gmail.com>
2023-06-22 11:24:54 +0700

    iterasi dari awal array hingga rear

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -70,11 +70,28 @@ public:
 
 		cout << "\nElements in the queue are: \n";
 
-		//Jika front_position <= rear position, iterasi dari FRONT hingga REAR
-		if (FRONT_position <= max - 1) {
-			cout << queue_array[FRONT_position] << "	";
-			FRONT_position++;
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "	";
+				FRONT_position++;
+			}
+			cout << endl;
 		}
 
-		FRONT_position = 0;
+		else {
+			//Jika front_position <= rear position, iterasi dari FRONT hingga REAR
+			while (FRONT_position <= max - 1) {
+			cout << queue_array[FRONT_position] << "	";
+			FRONT_position++;
+			}
 
+			FRONT_position = 0;
+
+			//iterasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+			cout << queue_array[FRONT_position] << "	";
+			FRONT_position++;
+			}
+			cout << endl;
+	}
+}
